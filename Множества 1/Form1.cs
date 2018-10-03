8e2978f4ee43eeea1f5e9bff21a8cd85dd6da633
Roman <papa-ap@list.ru>
2018-10-03 13:15:32 +0300

    ee

--- Множества 1/Form1.cs
+++ Множества 1/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,131 @@ namespace Множества_1
         public Form1()
         {
             InitializeComponent();
+            StoreColLection collection = new StoreColLection(@"Text.txt");
+            //collection.Add(2018);
+          // collection.Add(55);
+            collection.Remove(56);
+            foreach (int i in collection)
+            {
+                richTextBox1.Text += i.ToString() + " ";
+            }
         }
     }
+
+    public class StoreColLection : ICollection<int>
+    {
+        private readonly string _filePath;
+
+        public StoreColLection(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        private string [] GetNumbers()
+        {
+            string line = File.ReadAllText(_filePath);
+            string [] numbers = line.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries );
+            return numbers;
+        }
+
+        public int Count
+        {
+            get
+            {
+                string[] numbers = GetNumbers();
+                return numbers.Length;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        //void ICollection<int>.Add(int item) 
+     
+         public void Add(int item)
+        {
+            string[] numbers = GetNumbers();
+            if (numbers.Length == 0)
+                File.WriteAllText(_filePath, item.ToString());
+            else
+                File.AppendAllText(_filePath,"," + item.ToString());
+        }
+
+        public void Clear()
+        {
+            File.WriteAllText(_filePath," ");
+        }
+
+        public bool Contains(int item)
+        {
+            string[] numbers = GetNumbers();
+            foreach (string number in numbers)
+            {
+                if (Int32.Parse(number) == item)
+                    return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(int[] array, int arrayIndex)
+        {
+            string[] numbers = GetNumbers();
+            foreach (string number in numbers)
+            {
+                array[arrayIndex] = Int32.Parse(number);
+                arrayIndex++;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            string [] numbers = GetNumbers();
+
+            foreach (string number in numbers)
+            {
+                yield return Int32.Parse(number);
+            }
+        }
+
+        public bool Remove(int item)
+        {
+            string[] numbers = GetNumbers();
+            string line = File.ReadAllText(_filePath);
+
+            int SymbolPosition = 0;
+
+            foreach (string number in numbers)
+            {
+                if (Int32.Parse(number) == item)
+                {
+                    if (numbers.Length == 1)
+                    {
+                        line = " ";
+                    }
+                    else if (SymbolPosition == 0)
+                    {
+                        line = line.Substring(SymbolPosition + number.Length + 1);
+                    }
+                    else
+                    {
+                        line = line.Remove(SymbolPosition - 1, number.Length + 1);
+                    }
+                    File.WriteAllText(_filePath, line);
+                    return true;
+                }
+                SymbolPosition += number.Length + 1;
+            }
+
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
 }
--- Множества/Form1.cs
+++ Множества/Form1.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Множества_1;
 namespace Множества
 {
     
@@ -18,14 +18,47 @@ namespace Множества
         {
             InitializeComponent();
             Progression progression = new Progression(109);
+            if (false)richTextBox1.Text += "Игорь";richTextBox1.Text += "Игорь";
+                
+              
+            
+
             foreach (int i in progression)
             {
                 richTextBox1.Text += i.ToString();
             }
+            List<int> lst = new List<int>();
+            lst.Add(1);
+            lst.Add(2);
+            lst.Add(26);
+            lst.Remove(26);
+            richTextBox1.Text +=lst.Count;
+
+            Dictionary<int, Person> person = new Dictionary<int, Person>();
+            Person person1 = new Person() {Id=1,Name="Игорь" };
+            Person person2 = new Person() { Id = 2, Name = "Михаил" };
+            person.Add(person1.Id, person1);
+            person.Add(person2.Id, person2);
+            Person searchPerson;
+            bool isExist = person.TryGetValue(2,out searchPerson);
+            if (isExist)
+                richTextBox1.Text += searchPerson.Name;
+            int[] arr = new int[5];
+            IList<int> lst1 = arr;
+            // lst1.Add(1);
+            StoreColLection collection = new StoreColLection(@"Text.txt");
+            collection.Add(2018);
+            
         }
 
 
 
+
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
 
     }
     public class Progression : IEnumerable<int>
@@ -37,8 +70,18 @@ namespace Множества
             _itemCount = itemCount;
         }
         public IEnumerator<int> GetEnumerator()
-        {
-            return new ProgressionIterator(_itemCount);
+        { 
+            //"синтаксический сахар" yield return  он заменяет   итервейс IEnumerator 
+            int current = 1;
+            for (int i = 0; i < _itemCount - 101; i++)
+            {
+                if (i == 0) yield return 1;
+                current += 3;
+                yield return current;
+            }
+
+   
+          //  return new ProgressionIterator(_itemCount);
         }
         IEnumerator IEnumerable.GetEnumerator()
         {
